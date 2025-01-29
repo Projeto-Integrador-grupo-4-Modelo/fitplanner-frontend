b2f5673e3077ca6185a5c0cecf946848aa2773ce
Pribort <pribortniuk@gmail.com>
2025-01-29 15:49:14 -0300

    Pagina Home e about

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
