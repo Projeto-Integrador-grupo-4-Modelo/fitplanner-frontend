1b1bd6a2a922ba7c5c675e04b59448314eb6779c
Bruna <brunabosco@hotmail.com>
2025-01-29 17:19:41 -0300

    Estilização do Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+      <div>
         <Routes></Routes>
+      </div>
+      <Footer />  
       </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
